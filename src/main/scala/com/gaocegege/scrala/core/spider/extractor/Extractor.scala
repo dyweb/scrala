98eca02597de5cecae343f8f580161b1129db848
Gaoce <gaocegege@hotmail.com>
2015-11-09 10:39:19 +0800

    `

---
+++ src/main/scala/com/gaocegege/scrala/core/spider/extractor/Extractor.scala
@@ -0,0 +1,9 @@
+package com.gaocegege.scrala.core.spider.extractor
+
+/**
+ * TODO complete
+ * @author gaoce
+ */
+trait Extractor {
+  def matchRe(url: String)
+}
---
+++ src/main/scala/com/gaocegege/scrala/core/spider/rule/Rule.scala
@@ -0,0 +1,9 @@
+package com.gaocegege.scrala.core.spider.rule
+
+/**
+ * TODO complete
+ * @author gaoce
+ */
+class Rule {
+
+}
